d48a0c1243095260e5386e4b1be0567148ae1388
Ramon Boss <ramon.boss@outlook.com>
2016-12-20 12:34:54 +0100

    generated files

---
+++ cmake-build-debug/CMakeFiles/c_vigenere.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_vigenere.dir/main.c.o"
+  "c_vigenere.pdb"
+  "c_vigenere"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_vigenere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
