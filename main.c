09cad028ec39513a62f2e055b3ae25c939420cda
Ramon Boss <ramon.boss@outlook.com>
2016-12-31 10:46:36 +0100

    renamed variable and added default passp for testing

--- main.c
+++ main.c
@@ -1,31 +1,34 @@
 #include <stdlib.h>
+#include <memory.h>
 #include "vigenere.h"
 
 int main(int argc, char *argv[]) {
   char mode;
-  char *file;
+  char *fileName;
+
   if(argv[1][0] == '-') {
     mode = argv[1][1];
     if(mode == 'h') {
       hack(argv[2], argv[3]);
       return 0;
     }
-    file = argv[2];
+    fileName = argv[2];
   } else {
     mode = 'e';
-    file = argv[1];
+    fileName = argv[1];
   }
 
   char passPhrase[MAX_PASS_LEN] =  "abc123def456";
+  int passPhraseLen = 12;
 //  char passPhrase[MAX_PASS_LEN];
-//  askForPassPhrase(passPhrase);
+//  int passPhraseLen = askForPassPhrase(passPhrase);
 
   switch(mode) {
     case 'e':
-      encrypt(passPhrase, file);
+      encrypt(passPhrase, passPhraseLen, fileName);
       break;
     case 'd':
-      decrypt(passPhrase, file);
+      decrypt(passPhrase, passPhraseLen, fileName);
       break;
     default:
       exit(1);
