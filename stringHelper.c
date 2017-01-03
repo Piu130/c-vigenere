c3d7267e3b36cebd9b0fca3225c0560560231eac
Ramon Boss <ramon.boss@outlook.com>
2017-01-03 19:38:44 +0100

    commented endsWith

--- stringHelper.c
+++ stringHelper.c
@@ -2,6 +2,12 @@
 #include <memory.h>
 #include "stringHelper.h"
 
+/**
+ * Checks if a string ends with a suffix
+ * @param str    String to search for suffix
+ * @param suffix Suffix to search
+ * @return 1 if true else 0
+ */
 int endsWith(const char *str, const char *suffix)
 {
   if (!str || !suffix)
