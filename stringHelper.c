835c939567094b090a521b24a8fa4309861fa5f2
Ramon Boss <ramon.boss@outlook.com>
2017-01-04 14:49:44 +0100

    camelcased variable

--- stringHelper.c
+++ stringHelper.c
@@ -11,9 +11,9 @@
 int endsWith(const char *str, const char *suffix) {
   if (!str || !suffix)
     return 0;
-  size_t lenstr = strlen(str);
-  size_t lensuffix = strlen(suffix);
-  if (lensuffix >  lenstr)
+  size_t lenStr = strlen(str);
+  size_t lenSuffix = strlen(suffix);
+  if (lenSuffix >  lenStr)
     return 0;
-  return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+  return strncmp(str + lenStr - lenSuffix, suffix, lenSuffix) == 0;
 }
