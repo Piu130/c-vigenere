5401811ea3e53dcc8150f3e215f11583cc9dfdfd
Ramon Boss <ramon.boss@outlook.com>
2017-01-04 14:51:30 +0100

    commented function

--- fileHelper.c
+++ fileHelper.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "fileHelper.h"
 
+/**
+ * Returns the size of a file
+ * @param file The file to detect the size
+ * @return Size of the file
+ */
 long fsize(FILE *file) {
   fseek(file, 0L, SEEK_END);
   long decryptedFileSize = ftell(file);
