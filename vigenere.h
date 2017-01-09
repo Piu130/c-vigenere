742dcf8917b39d51d89aa0c9fc8a557c6d0c4f33
Ramon Boss <ramon.boss@outlook.com>
2017-01-09 20:02:38 +0100

    added const to unchanged char*

--- vigenere.h
+++ vigenere.h
@@ -4,8 +4,8 @@
   #define MAX_PASS_LEN 20
 
   void askForPassPhrase(char *);
-  void encrypt(char *, char *);
-  void decrypt(char *, char *);
-  void hack(char *, char *);
+  void encrypt(const char *, const char *);
+  void decrypt(const char *, const char *);
+  void hack(const char *, const char *);
 
 #endif //C_VIGENERE_VIGENERE_H
